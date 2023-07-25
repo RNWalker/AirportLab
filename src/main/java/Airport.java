0c3df713ee449afc06c3471421ddb7ede81c761b
RNWalker <rebecca.noelia.walker@gmail.com>
2023-07-25 14:55:57 +0100

    airport class

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -1,2 +1,39 @@
+import java.util.ArrayList;
+
 public class Airport {
+
+    private ArrayList<Flight> flights;
+    private String name;
+
+    public Airport(String name){
+        this.name = name;
+        this.flights = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<Flight> getFlights() {
+        return this.flights;
+    }
+
+    public void cancelFlight(Flight cancelledFlight){
+        this.flights.remove(cancelledFlight);
+    }
+
+    public void addFlight(Flight addedFlight){
+        this.flights.add(addedFlight);
+    }
+
+    public ArrayList<Flight> displayAllFlights(){
+        for (Flight flight: flights){
+            System.out.println(flight);
+        }
+        return this.flights;
+    }
+
+    public int countFlights(){
+        return this.flights.size();
+    }
 }
--- src/test/java/AirportTest.java
+++ src/test/java/AirportTest.java
@@ -1,22 +1,53 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class AirportTest {
 
-//    Airport airport = new Airport();
+Airport airport;
+Flight flight1;
+Flight flight2;
 
-//    airport = (details);
+@BeforeEach
+public void setUp(){
+    airport = new Airport("Heathrow");
+    flight1 = new Flight("Bali", "KA29");
+    flight2 = new Flight("Darkest Peru", "PAD70");
+    airport.addFlight(flight1);
+    airport.addFlight(flight2);
+}
 
-    //    @Test
-//    public String canGetName()
+@Test
+public String canGetName(){
 
-    //    @Test
-//    public int canGetFlights()
+    return airport.getName();
+}
 
-    //    @Test
-//    public int canCancelFlight()
+@Test
+public ArrayList<Flight> canGetFlights(){
 
-    //    @Test
-//    public int canDisplayAllFlights()
+    return airport.getFlights();
+}
 
-    //    @Test
-//    public int canAddFlightToCollection()
+@Test
+public void canCancelFlight(){
+    airport.cancelFlight(flight1);
+    assertThat(airport.countFlights()).isEqualTo(1);
+}
+
+@Test
+public void canAddFlight(){
+    Flight flight = new Flight("Spain", "1234");
+    airport.addFlight(flight);
+    assertThat(airport.countFlights()).isEqualTo(3);
+}
+
+@Test
+    public void canDisplayAllFlights(){
+    airport.displayAllFlights();
+}
 
 }
